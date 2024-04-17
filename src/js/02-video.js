293da96a30809c8ab80186ef0203b19aff7ddc9a
lassandra85 <lassandra85@gmail.com>
2024-04-17 11:56:18 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,21 @@
 import Player from '@vimeo/player';
+/* import throttle from 'lodash.throttle'; */
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
+console.log(player);
 
-player.on('play', function () {
-  console.log('played the video!');
+const currentVideoTime =
+  JSON.parse(localStorage.getItem('videoplayer - current - time')) || 0;
+
+console.log(currentVideoTime);
+
+player.on('timeupdate', function ({ seconds }) {
+  localStorage.setItem('videoplayer - current - time', JSON.stringify(seconds));
 });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+/* localStorage.clear(); */
